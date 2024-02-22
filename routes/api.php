48adf3859b43787634f5cea9ba7482dbff6695e0
nasser <hekmati.nasser@gmail.com>
2024-02-22 23:03:23 +0330

    feat: api auth

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('/auth')->group(function () {
+    Route::post('register', [AuthenticationController::class, 'register'])->name('register');
+    Route::post('login', [AuthenticationController::class, 'login'])->name('login');
+    Route::post('forget', [AuthenticationController::class, 'forget'])->name('forget');
+    Route::post('reset', [AuthenticationController::class, 'reset'])->name('reset');
+    Route::post('set-password', [AuthenticationController::class, 'setPassword'])->name('set-password');
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('/auth')->group(function () {
+        Route::get('profile', [AuthenticationController::class, 'profile'])->name('profile');
+        Route::post('change-password', [AuthenticationController::class, 'changePassword'])->name('change-password');
+        Route::post('logout', [AuthenticationController::class, 'logout'])->name('logout');
+    });
+
+
+});
+
