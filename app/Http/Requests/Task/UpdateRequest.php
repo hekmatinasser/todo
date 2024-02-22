5e3ed71bf98941d7efbeec7534f0acf42c3511fb
nasser <hekmati.nasser@gmail.com>
2024-02-23 01:11:25 +0330

    fix: update request

---
+++ app/Http/Requests/Task/UpdateRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name'        => ['required', 'string', 'max:190'],
+            'description' => ['nullable']
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return auth()->user()->can('update', $this->route('task'));
+    }
+}
