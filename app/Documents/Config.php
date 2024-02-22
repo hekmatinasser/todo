17179aa3dfb7de0b5ac54330a26fff5a9aeb5720
nasser <hekmati.nasser@gmail.com>
2024-02-22 21:03:48 +0330

    feat: config swagger

---
+++ app/Documents/Config.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Documents;
+
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="Integration APIs"
+ * )
+ *
+ * @OA\PathItem(path="/api")
+ *
+ *
+ * @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      type="http",
+ *      scheme="bearer"
+ * )
+ */
+
+class Config
+{
+
+}
