ec2a98ca1708ed99a2f1475083afd49c42e15842
nasser <hekmati.nasser@gmail.com>
2024-02-23 01:11:17 +0330

    fix: store request

---
+++ app/Http/Requests/Task/StoreRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+use App\Models\Task;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name'        => ['required', 'string', 'max:190'],
+            'description' => ['nullable']
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return auth()->user()->can('create', Task::class);
+    }
+}
