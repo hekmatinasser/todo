dfcfde1e5eb63e47c09d7e166692b3fa59b6b0dc
nasser <hekmati.nasser@gmail.com>
2024-02-23 01:14:04 +0330

    fix: task repository index

--- app/Repositories/Task/TaskRepository.php
+++ app/Repositories/Task/TaskRepository.php
@@ -4,10 +4,16 @@ namespace App\Repositories\Task;
 
 use App\Models\Task;
 use Celysium\Base\Repository\BaseRepository;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class TaskRepository extends BaseRepository implements TaskRepositoryInterface
 {
+
+    public function query(Builder $query, array $parameters): Builder
+    {
+        return $query->where('user_id', auth()->id());
+    }
 
     public function store(array $parameters): Model
     {
